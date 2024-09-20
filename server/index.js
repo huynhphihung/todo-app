e47d76d7d3f8a11f77a49397f3d11959996d5638
huynhphihung <phihunghuynh2005@gmail.com>
2024-09-20 20:00:39 +0700

    second deployment

--- server/index.js
+++ server/index.js
@@ -17,7 +17,7 @@ const URI = process.env.DB_URI || ""
 
 const __dirname = path.resolve()
 
-app.use(cors({origin: "http://localhost:5173", credentials: true}));
+app.use(cors({origin: "https://todo-app-g7v3.onrender.com", credentials: true}));
 app.use(bodyParser.urlencoded({extended: true, limit: "30mb"}))
 app.use(bodyParser.json({limit: "30mb"}))
 app.use(express.json({limit: "30mb"}))
